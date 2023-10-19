9fa287cb1909ddf78ad5e02f03c935966e86fdaf
ka <todo1@rotarydef.net>
2023-10-19 08:50:22 -0700

    fix miss : condition when nan is returned

--- merge/merge.hpp
+++ merge/merge.hpp
@@ -28,93 +28,86 @@ struct merge{
     merge(preference * pref) : pref(pref){}
     ~merge(){}
     
-    
     void set_diff(value_type v){ diff = v; }
-    
-    //@note not used
-    bool is_contained(offset_type pos , int direction){
-        return bool(
-            !direction
-            +(2 == ( bool(pos % 16)  + (direction<0)))
-            +(2 == (!bool(pos % 16) + (direction>0)))
-        );
-    };
-    
-    
-    
-    int is_same_section(typename bt_type::btree_search_result const& btres0,typename  bt_type::btree_search_result const& btres1){
+    int exec(value_type from,value_type to){
         
-        // if  nearest_pos is same, then
-            // return -1 (case in the same vacant)
-                // 1) !nearest_pos % 16 &&  both direction < 0 && (both is not exact)
-                // 2) nearest_pos % 16 &&  both direction > 0 && (both is not exact)
-            // return 1 (case in the same block)
-                // 1) !nearest_pos % 16 &&  both direction > 0
-                // 2) nearest_pos % 16 &&  both direction < 0
-        // otherwise return 0 (two exist in different block) 
+        auto is_contained = [](offset_type pos , int direction)->bool{
+            return bool(
+                !direction
+                +(2 == ( bool(pos % 16)  + (direction<0)))
+                +(2 == (!bool(pos % 16) + (direction>0)))
+            );
+        };
         
-        auto is_same_pos = btres0.nearest_pos == btres1.nearest_pos; 
-        auto is_even = !(btres0.nearest_pos%(sizeof(value_type)*2) ); 
+        auto is_same_section = [](typename bt_type::btree_search_result const& btres0,typename  bt_type::btree_search_result const& btres1)->int{
+            
+            // if  nearest_pos is same, then
+                // return -1 (case in the same vacant)
+                    // 1) !nearest_pos % 16 &&  both direction < 0 && (both is not exact)
+                    // 2) nearest_pos % 16 &&  both direction > 0 && (both is not exact)
+                // return 1 (case in the same block)
+                    // 1) !nearest_pos % 16 &&  both direction > 0
+                    // 2) nearest_pos % 16 &&  both direction < 0
+            // otherwise return 0 (two exist in different block) 
+            
+            auto is_same_pos = btres0.nearest_pos == btres1.nearest_pos; 
+            auto is_even = !(btres0.nearest_pos%(sizeof(value_type)*2) ); 
+            
+            // -2 <= directions <= 2  direction==0 is counted as contained. 
+            auto cond_exact = !btres0.direction+!btres1.direction;
+            auto directions = btres0.direction+btres1.direction+cond_exact;
+            
+            return is_same_pos *(
+                  bool((is_even * directions==-2) +(!is_even * directions==2))*!bool(cond_exact)*-1   
+                + bool( is_even * directions==2 +!is_even * directions==-2)*1  
+            );
+        };
         
-        // -2 <= directions <= 2  direction==0 is counted as contained. 
-        auto cond_exact = !btres0.direction+!btres1.direction;
-        auto directions = btres0.direction+btres1.direction+cond_exact;
-        
-        return is_same_pos *(
-              bool((is_even * directions==-2) +(!is_even * directions==2))*!bool(cond_exact)*-1   
-            + bool( is_even * directions==2 +!is_even * directions==-2)*1  
-        );
-    };
-    
-                    
-    offset_type adjust_np_ovf(typename bt_type::btree_search_result const& i0,offset_type pos_min,offset_type  pos_max){
-            // if upper overflow then nearest_pos = max_size   
-            // if lower overflow then nearest_pos = mix_size   
-            return 
-                  i0.overflow*((i0.direction > 0)*pos_max + (i0.direction < 0)*pos_min)
-                +!i0.overflow*i0.nearest_pos;
-    };
-    
-    // adjust direction with specified diff
-    bool is_adjust_diff(typename bt_type::btree_search_result const& i0,value_type diff,value_type input){
-        auto i0_diff=
-              (i0.nearest_value > input)*(i0.nearest_value-input)
-            +!(i0.nearest_value > input)*(-i0.nearest_value+input);
-        return (i0_diff <= diff);
-    };
-
-    
-    
-    void __a(value_type from,value_type to){
-        auto is_ordered = from<to;
-        auto bt = kautil::algorithm::btree_search{pref};
-        auto i0 = bt.search(from,false);
-        auto i1 = bt.search(to,false);
-        
+                        
+        auto adjust_np_ovf = [](typename bt_type::btree_search_result const& i0,offset_type pos_min,offset_type  pos_max)->offset_type{
+                // if upper overflow then nearest_pos = max_size   
+                // if lower overflow then nearest_pos = mix_size   
+                return 
+                      i0.overflow*((i0.direction > 0)*pos_max + (i0.direction < 0)*pos_min)
+                    +!i0.overflow*i0.nearest_pos;
+        };
         
         // adjust direction with specified diff
-        auto i0_is_diff_adjust = is_adjust_diff(i0,diff,from);
-        i0.direction*=!i0_is_diff_adjust;
-        i0.overflow*=!i0_is_diff_adjust;
-        
-        auto i1_is_diff_adjust = is_adjust_diff(i1,diff,to);
-        i1.direction*=!i1_is_diff_adjust;
-        i1.overflow*=!i1_is_diff_adjust;
-
-        constexpr auto kSameBlock = 1,kDifferent = 0,kSamevacant = -1;
-        auto cond_section = is_same_section(i0,i1);
-        if(0==(!is_ordered+!(kSameBlock !=cond_section))){ 
-            // newly add element to memory or file
-            auto is_claim_region = bool(kSamevacant==cond_section);  
-            auto fsize=  pref->size();
-            if(is_ordered*!fsize){
-                printf("write block : from,to(%lld,%lld)",from,to);
-                value_type new_block[2]= {from,to};
-                auto new_block_ptr = &new_block;
-                pref->write(0,(void**)&new_block_ptr,sizeof(*new_block));
-//            }else if(0==(!is_ordered+!(fsize == (sizeof(value_type)*2))) ){
-            }else if(2==(is_ordered+!(fsize % (sizeof(value_type)*2))) ){
+        auto is_adjust_diff = [](typename bt_type::btree_search_result const& i0,value_type diff,value_type input)->bool{
+            auto i0_diff=
+                  (i0.nearest_value > input)*(i0.nearest_value-input)
+                +!(i0.nearest_value > input)*(-i0.nearest_value+input);
+            return (i0_diff <= diff);
+        };
     
+        if(false==from<to) return 1;
+
+        auto fsize=  pref->size();
+        if(!fsize){
+            printf("write block : from,to(%lld,%lld)",from,to);
+            value_type new_block[2]= {from,to};
+            auto new_block_ptr = &new_block;
+            pref->write(0,(void**)&new_block_ptr,sizeof(*new_block));
+            return 0;
+        }else if(!(fsize % (sizeof(value_type)*2)) ){
+            auto bt = kautil::algorithm::btree_search{pref};
+            auto i0 = bt.search(from,false);
+            auto i1 = bt.search(to,false);
+            constexpr auto kSameBlock = 1,kDifferent = 0,kSamevacant = -1;
+            auto cond_section = is_same_section(i0,i1);
+            if(kSameBlock !=cond_section){ // newly add element to memory or file
+                auto is_claim_region = bool(kSamevacant==cond_section);  
+            
+                // adjust direction with specified diff
+                auto i0_is_diff_adjust = !i0.nan*is_adjust_diff(i0,diff,from);
+                i0.direction*=!i0_is_diff_adjust;
+                i0.overflow*=!i0_is_diff_adjust;
+                
+                auto i1_is_diff_adjust = !i1.nan*is_adjust_diff(i1,diff,to);
+                i1.direction*=!i1_is_diff_adjust;
+                i1.overflow*=!i1_is_diff_adjust;
+                
+                
                 auto squash_src=offset_type(0),squash_length=offset_type(0);
                 auto pos_min = 0;
                 auto pos_max = pref->size()-sizeof(value_type);
@@ -129,7 +122,6 @@ struct merge{
                     // if lower overflow then 
                         // nearest_pos = mix_size   
                         // nearest_value = from
-                    
                         
                     i0.nearest_pos = adjust_np_ovf(i0,pos_min,pos_max);
                     i0.nearest_value = i0.overflow*from + !i0.overflow*i0.nearest_value; 
@@ -207,8 +199,6 @@ struct merge{
                 }
 
                 
-                
-                
                 auto is_ovf_ovf_upper = (2==(i0.overflow+(i0.direction>0))); 
                 auto is_ovf_ovf_lower = (2==(i1.overflow+(i1.direction<0))); 
                 {// adjust when ovf_ovf
@@ -245,22 +235,37 @@ struct merge{
                     auto i1_ptr = &i1.nearest_value;
                     pref->write(i1.nearest_pos,(void**)&i1_ptr,sizeof(value_type));
                 }
-            }else{
-                printf("else\n",fsize);
-                printf("  %d\n",fsize);
+                
+                printf("output result\n");
+                debug_out_file<value_type,offset_type>(stdout,pref->fd,0,100);
+                printf("+++++++++++++++++++++++++++++++++++++++++++\n");
+                return 0;
             }
-        }else{
-            printf("wrong corrupted cache data\n");
-            printf("  is_ordered(%d)\n",is_ordered);
+        }else return 2;
+        return 3;
+    }
+    
+    const char* error_msg(int err){
+        switch (err) {
+            case 1: {
+                static const char * kMsg = "error(2) : invalid input";
+                return kMsg;
+            }; 
+            case 2:
+            {
+                static const char * kMsg = "error(1) : corrupted region.";
+                return kMsg;
+            }; 
+            case 3: {
+                static const char * kMsg = "error(3) :unknown error";
+                return kMsg;
+            }; 
+            default: {
+                static const char * kMsg = "not unerror";
+                return kMsg;
+            };
+            
         }
-        
-        
-        printf("output result\n");
-        debug_out_file<value_type,offset_type>(stdout,pref->fd,0,100);
-        printf("+++++++++++++++++++++++++++++++++++++++++++\n");
-        
-        
-        
     }
     
     
@@ -283,9 +288,9 @@ int temp() {
     auto to = value_type(0);
     
     
-    { // case single block
-        v.resize(2);
-            diff = value_type(0);
+//    { // case single block
+//        v.resize(2);
+//            diff = value_type(0);
 //            from = value_type(0);to = value_type(10);
 //            from = value_type(15);to = value_type(30);
 //            from = value_type(0);to = value_type(30);
@@ -295,15 +300,15 @@ int temp() {
 //            from = value_type(15);to = value_type(17);
 //            from = value_type(5);to = value_type(9);
 //            from = value_type(31);to = value_type(35);
-    }
+//    }
 
-//    {
-//        v.resize(2);
-//        diff = 1;
+    {
+        v.resize(2);
+        diff = 1;
 //    // ovf-contained expect ([0,8] - (1,20)) 
 //        from = value_type(1);to = value_type(11);  // todo  miss : [0,8] - (1,20)
-//        from = value_type(1);to = value_type(9); 
-//        
+        from = value_type(1);to = value_type(9); 
+
 //    // contained-ovf 
 //        from = value_type(9);to = value_type(21); //expect [0,8] (10,20)  todo  miss : [0,8] - (9,21) 
 //        from = value_type(8);to = value_type(22); //expect [0,8] (8,22)
@@ -319,8 +324,8 @@ int temp() {
 //    // ovf(lower) ovf(lower) 
 //        from = value_type(21);to = value_type(35); // expect [0,8] : (10,35)     
 //        from = value_type(22);to = value_type(35); // expect [16,24] : (22,35)
-//        
-//    }
+
+    }
     
 //    
 //    { // case two block 
@@ -389,7 +394,7 @@ int temp() {
     auto m = merge{&pref};
     //m.__a(0,20);
     m.set_diff(diff);
-    m.__a(from,to); // 0,20 => cont-overflow expect [0,8] (0,30) 
+    m.exec(from,to); // 0,20 => cont-overflow expect [0,8] (0,30) 
     fclose(f);
         
     
